b8104d8445524f99e8b68b5c061d62e4882d5e38
student <student@Veena-2.local>
2016-08-02 12:29:00 +0530

    Swift file

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  pushSegue
+//
+//  Created by student on 14/07/16.
+//  Copyright © 2016 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func gotosecond(sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
